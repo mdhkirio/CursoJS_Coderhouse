9b3b7a64460002877a41ca037435e250c83e3df0
Skynet's iMac <mauriciohernandez@Mauricios-iMac.local>
2022-10-24 23:50:22 -0500

    entrega

---
+++ js/script.js
@@ -0,0 +1,132 @@
+let opcion;
+let cineElegido;
+
+function buscarCine() 
+{
+    let preguntarCine;
+    let cine;
+
+    preguntarCine = parseInt(prompt("Cines disponibles \n1.- Cinepolis \n2.- Cinemex \n3.- Cine las Américas \n4.- Cine Alameda"));
+    
+    switch (preguntarCine)
+    {
+        case 1:
+            cine = "Cinepolis";
+            break;
+        case 2:
+            cine = "Cinemex";
+            break;
+        case 3:
+            cine = "Cine las Américas";
+            break;
+        case 4:
+            cine = "Cine Alameda";
+            break;            
+        default:
+            cine = ""
+            break;
+    }
+    return cine;
+}
+
+function buscarPelicula(cine)
+{
+    let opcionPelicula;
+    let pelicula;
+
+    switch (cine)
+    {
+        case "Cinepolis":
+            opcionPelicula = parseInt(prompt("Películas disponibles \n1.- Harry Potter \n2.- Animales Fantásticos"));
+            switch (opcionPelicula)
+            {
+                case 1:
+                    pelicula = "Harry Potter"
+                    break;
+                case 2:
+                    pelicula = "Animales Fantásticos"
+                    break;
+                default:
+                    pelicula = ""
+                    break;
+            }
+            break;
+        case "Cinemex":
+            opcionPelicula = parseInt(prompt("Películas disponibles \n1.- 007 \n2.- Superman"));
+            switch (opcionPelicula)
+            {
+                case 1:
+                    pelicula = "007"
+                    break;
+                case 2:
+                    pelicula = "Superman"
+                    break;
+                default:
+                    pelicula = ""
+                    break;
+            }
+            break;
+        case "Cine las Américas":
+            opcionPelicula = parseInt(prompt("Películas disponibles \n1.- El Rey León \n2.- El Rey Escorpión"));
+            switch (opcionPelicula)
+            {
+                case 1:
+                    pelicula = "El Rey León"
+                    break;
+                case 2:
+                    pelicula = "El Rey Escorpión"
+                    break;
+                default:
+                    pelicula = ""
+                    break;
+            }
+            break;
+        case "Cine Alameda":
+            opcionPelicula = parseInt(prompt("Películas disponibles \n1.- Avengers \n2.- Los 4 Fantásticos"));
+            switch (opcionPelicula)
+            {
+                case 1:
+                    pelicula = "Avengers"
+                    break;
+                case 2:
+                    pelicula = "Los 4 Fantásticos"
+                    break;
+                default:
+                    pelicula = ""
+                    break;
+            }
+            break;        
+    }
+    return pelicula;
+}
+
+do
+{
+    alert("Busca tu próxima aventura \nPaso 1 - Elige tu cine \nPaso 2 - Elige tu película \nPaso 3 - Compra tus boletos");
+
+    opcion = parseInt(prompt("1.- Deseas continuar \n2.- Salir"));
+
+    switch (opcion)
+    {
+        case 1:
+            cineElegido = buscarCine();
+            if (cineElegido != "")
+            {
+                buscarPelicula(cineElegido);
+            } else
+            {
+                opcion = 3
+            }
+            break;
+        default:
+            break;
+    }
+
+    if (opcion < 1 || opcion > 2)
+    {
+        alert("opción inválida");
+    }
+} 
+while (opcion < 1 || opcion > 2);
+
+alert("Gracias por visitarnos, te esperamos pronto");
